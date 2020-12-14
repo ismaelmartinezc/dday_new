c8d34a8723a5472183a78c3bcba3cd6b8eb7ae5a
Ismael Martínez <ismael.martinez@outlook.cl>
2020-12-14 00:14:58 -0300

    mapvote try

--- g_cmds.c
+++ g_cmds.c
@@ -236,7 +236,8 @@ g_cmds_t id_GameCmds[NUM_ID_CMDS] = // remember to set back to NUM_ID_COMDS
 	"spot", 1, Cmd_Spot,//for mappers to find coordinates for map entities
 	"removedoors", 1, Cmd_RemoveDoors,
 	"showlinks", 1, ShowLinks,
-	"removenode", 1, SV_Remove_Node
+	"removenode", 1, SV_Remove_Node,
+    "vote", 1, MapVote
 };
 
 // MENUS
